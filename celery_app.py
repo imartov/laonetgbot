dfb5ca9b3c7538ab1653c10cb793fb8a0be8d2c2
imartov <alexandr.kosyrew@mail.ru>
2024-03-17 15:23:05 +0300

    correct docker-compose.yml

--- celery_app.py
+++ celery_app.py
@@ -5,18 +5,23 @@ from functools import wraps
 from app import service_send_info
 
 
-app = Celery('celery_app', broker='pyamqp://guest@localhost//')
+# app = Celery('celery_app', broker='pyamqp://guest@localhost//')
+app = Celery('celery_app', broker='pyamqp://guest@rabbit//') # Docker
+
+# app = Celery('celery_app', 'amqp://laoneuser:laonepassword@localhost:5672//')
+
 # app = Celery('celery_app', broker='redis://localhost:6379/1')
+# app = Celery('celery_app', broker='redis://redis:6379/0') # Docker
 # app = Celery('celery_app', 'amqp://guest@localhost//')
+
 app.conf.timezone = 'UTC'
 app.control.purge()
 
 
 @app.task
 def periodic_send_info():
-    asyncio.get_event_loop().run_until_complete(service_send_info())
-    asyncio.run(service_send_info())
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(service_send_info())
 
 app.conf.beat_schedule = {
    "periodic_rask": {
--- db.py
+++ db.py
@@ -18,8 +18,10 @@ load_dotenv()
 
 # TODO: использовать .env вместо config
 # engine = create_engine(f'postgresql+psycopg2://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOSTNAME")}/{os.getenv("DB_NAME")}')
+
 # engine = create_engine(f'postgresql+psycopg2://laoneuser:laonepassword@localhost:5432/laonedb')
-engine = create_engine(f'postgresql+psycopg2://laoneuser:laonepassword@postgres:5432/laonedb')
+engine = create_engine(f'postgresql+psycopg2://laoneuser:laonepassword@postgres:5432/laonedb') # Docker
+
 metadata_obj = MetaData()
 
 product_requests = Table(
